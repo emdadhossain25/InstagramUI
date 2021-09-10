03a0ea98d60da6c5380f44f819a16fc0c69d7406
unknown <you@example.com>
2021-09-11 01:47:47 +0600

    removed pre built code

--- app/src/main/java/com/emdad/instagramui/MainActivity.kt
+++ app/src/main/java/com/emdad/instagramui/MainActivity.kt
@@ -14,25 +14,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InstagramUITheme {
-        Greeting("Android")
     }
 }
